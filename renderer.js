938479d44ba826f45beee49f17a7c58b7284c600
Tim Cross <theophilusx@gmail.com>
2018-01-02 15:50:21 +1100

    Re-factor ticking of seen/heard words

--- renderer.js
+++ renderer.js
@@ -13,39 +13,6 @@ btn.addEventListener('click', function (e) {
   ipcRenderer.send('game-request', 'new-game');
 });
 
-// function makeParentCell(data, idx) {
-//     let cell = document.createElement('div');
-//     if (data === null) {
-//         cell.className = 'game-cell blank-cell';
-//     } else {
-//         if (idx % 2 === 0) {
-//             cell.className = `game-cell dark cell-${idx}`;
-//         } else {
-//             cell.className = `game-cell darkest cell-${idx}`;
-//         }
-//         cell.id = `cell-${idx}`;
-//     }
-//     return cell;
-// }
-
-// function makeDataCell(data, idx) {
-//     let cell = document.createElement('div');
-//     let cellText = document.createTextNode(data);
-
-//     cell.className = `game-data cell-${idx}`;
-//     cell.appendChild(cellText);
-//     return cell;
-// }
-
-// function makeGameCell(data, idx) {
-//     let cell = makeParentCell(data, idx);
-//     if (data !== null) {
-//         let cellData = makeDataCell(data, idx);
-//         cell.appendChild(cellData);    
-//     }
-//     return cell;
-// }
-
 function makeGameCell(data, idx) {
   let cell = document.createElement('div');
   if (data === null) {
@@ -59,46 +26,42 @@ function makeGameCell(data, idx) {
   return cell;
 }
 
-ipcRenderer.on('game-data', function (evt, msg) {
-  var game = JSON.parse(msg);
-  var gameParent = document.querySelector('div.game-card');
-  gameParent.innerHTML = null;
+function makeGameCard(parent, words) {
   var row;
-  for (let i = 0; i < game.length; i++) {
+  parent.innerHTML = null;
+  for (let i = 0; i < words.length; i++) {
     if (i % 5 === 0) {
       if (row !== undefined) {
-        gameParent.appendChild(row);
+        parent.appendChild(row);
       }
-      row = document.createElement('div'); 
+      row = document.createElement('div');
       row.className = 'row';
     }
-    row.appendChild(makeGameCell(game[i], i));
+    row.appendChild(makeGameCell(words[i], i));
   }
-  gameParent.appendChild(row);
+  parent.appendChild(row);
+}
+
+ipcRenderer.on('game-data', function (evt, msg) {
+  var gameWords = JSON.parse(msg);
+  var gameParent = document.querySelector('div.game-card');
+  gameParent.innerHTML = null;
+  makeGameCard(gameParent, gameWords);
 });
-
-// function getCellId(evt) {
-//     if (evt.target && evt.target.nodeName == 'DIV') {
-//         let cellId = evt.target.className
-//                      .split(' ')
-//                      .filter(w => w.startsWith('cell-'))[0];
-//         return cellId;
-//     }
-// }
-
-// function getCell(evt) {
-//     var cellId = getCellId(evt);
-
-//     if (cellId !== undefined) {
-//         return document.getElementById(cellId);
-//     }
-// }
 
 function getCell(evt) {
   var cellId = evt.target.id;
 
   if (cellId !== undefined) {
     return document.getElementById(cellId);
+  }
+}
+
+function toggleSeen(cell) {
+  if (cell.className.includes('seen-word')) {
+    cell.className = 'cell-data';
+  } else {
+    cell.className = 'cell-data seen-word';
   }
 }
 
@@ -111,9 +74,6 @@ gameDiv.addEventListener('click', function (evt) {
   evt.stopPropagation();
   cellElement = getCell(evt);
   if (cellElement !== undefined) {
-    cellElement.style = 'background-image:url(check-mark.svg);' +
-      'background-position:right bottom;' +
-      'background-size:20px;' +
-      'background-repeat: no-repeat';
+    toggleSeen(cellElement);
   }
 });
