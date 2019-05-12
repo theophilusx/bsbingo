5732fa067b5799b531be1e51cc520f459e4bffbe
Tim Cross <theophilusx@gmail.com>
2019-05-12 19:18:00 +1000

    Reformat and convert to es5 modules

--- main.js
+++ main.js
@@ -1,156 +1,164 @@
 /* jshint esversion: 6, node: true */
-'use strict';
+"use strict";
 
-const electron = require('electron');
-const path = require('path');
-const url = require('url');
+import electron from "electron";
+import { join } from "path";
+import { format } from "url";
 
 // set environment
-process.env.NODE_ENV = 'development';
+process.env.NODE_ENV = "development";
 
-const {app, BrowserWindow, Menu, ipcMain} = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 var mainWindow;
 
 // define menus
-var mainMenuTemplate = [{
-    label: 'File',
-    submenu: [{
-        label: 'Quit',
-        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+var mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
         click() {
-            app.quit();
+          app.quit();
         }
-    }]
-}];
+      }
+    ]
+  }
+];
 
 // add empty entry to get OSX menus looking right
-if (process.platform == 'darwin') {
-    mainMenuTemplate.unshift({});
+if (process.platform == "darwin") {
+  mainMenuTemplate.unshift({});
 }
 
 // add developer menu item if not prod version
-if (process.env.NODE_ENV != 'production') {
-    mainMenuTemplate.push({
-        label: 'Developer Tools',
-        submenu: [{
-            role: 'reload'
-        },
-        {
-            label: 'Toggle DevTools',
-            accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
-            click(item, focusedWindow) {
-                focusedWindow.toggleDevTools();      
-            }
-        }]        
-    });
+if (process.env.NODE_ENV != "production") {
+  mainMenuTemplate.push({
+    label: "Developer Tools",
+    submenu: [
+      {
+        role: "reload"
+      },
+      {
+        label: "Toggle DevTools",
+        accelerator: process.platform == "darwin" ? "Command+I" : "Ctrl+I",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      }
+    ]
+  });
 }
 
 function createMainWindow() {
-    var mainMenu;
+  var mainMenu;
 
-    mainWindow = new BrowserWindow({width: 900, height: 660});
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-    mainWindow.on('close', function() {
-        // For OSX UX consistency, don't quit
-        // Set window handler to null
-        mainWindow = null;
-    });
-    mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-    Menu.setApplicationMenu(mainMenu);
+  mainWindow = new BrowserWindow({ width: 900, height: 660 });
+  mainWindow.loadURL(
+    format({
+      pathname: join(__dirname, "index.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+  mainWindow.on("close", function() {
+    // For OSX UX consistency, don't quit
+    // Set window handler to null
+    mainWindow = null;
+  });
+  mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 }
 
-app.on('ready', createMainWindow);
+app.on("ready", createMainWindow);
 
-app.on('window-all-closed', function() {
-    // OSX UX keeps app running even when all windows
-    // are closed
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on("window-all-closed", function() {
+  // OSX UX keeps app running even when all windows
+  // are closed
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
 
-app.on('activate', function() {
-    // Handle OSX clicking on dock icon when app 
-    // still running
-    if (mainWindow === null) {
-        createMainWindow();
-    }
+app.on("activate", function() {
+  // Handle OSX clicking on dock icon when app
+  // still running
+  if (mainWindow === null) {
+    createMainWindow();
+  }
 });
 
-// Game 
+// Game
 
-const {getRandomInt} = require('./util.js');
-const {createReadStream} = require('fs');
+import { getRandomInt } from "./util.js";
+import { createReadStream } from "fs";
 
 // Read file of bingo words. Return array of words
-// Using a Promise to do this is like hunting rabbits with 
-// a bazooka! However, this is deliberate as this functionality 
+// Using a Promise to do this is like hunting rabbits with
+// a bazooka! However, this is deliberate as this functionality
 // will likely be changed to an ajax call in the future.
 function getWordList(filename) {
-    return new Promise(function(resolve, reject) {
-        var tmp = "", wordArray = [];
+  return new Promise(function(resolve, reject) {
+    var tmp = "",
+      wordArray = [];
 
-        createReadStream(path.join(__dirname, filename), {encoding: 'UTF-8'})
-            .on('data', chunk => tmp += chunk)
-            .on('error', err => reject(err))
-            .on('end', function() {
-                wordArray = tmp.split('\n').filter(w => w.length > 0);
-                resolve(wordArray);
-            });
-    });
+    createReadStream(join(__dirname, filename), { encoding: "UTF-8" })
+      .on("data", chunk => (tmp += chunk))
+      .on("error", err => reject(err))
+      .on("end", function() {
+        wordArray = tmp.split("\n").filter(w => w.length > 0);
+        resolve(wordArray);
+      });
+  });
 }
 
 // Generate a game of 'size' words with 'blanks' being
 // blank from the list of available words in wordArray
 function getGameWords(wordArray, size, blanks) {
-    var min = 0;
-    var max = wordArray.length;
-    var gameSet = new Set();
-    var blankSet = new Set();
-    var gameWords = [];
+  var min = 0;
+  var max = wordArray.length;
+  var gameSet = new Set();
+  var blankSet = new Set();
+  var gameWords = [];
 
-    // build up list of random words
-    while (gameSet.size < size) {
-        gameSet.add(getRandomInt(min, max));
-    }
-    for (let id of gameSet) {
-        gameWords.push(wordArray[id]);
-    }
+  // build up list of random words
+  while (gameSet.size < size) {
+    gameSet.add(getRandomInt(min, max));
+  }
+  for (let id of gameSet) {
+    gameWords.push(wordArray[id]);
+  }
 
-    // We want some word slots to be blank
-    while (blankSet.size < blanks) {
-        blankSet.add(getRandomInt(min, size));
-    }
-    for (let id of blankSet) {
-        gameWords[id] = undefined;
-    }
+  // We want some word slots to be blank
+  while (blankSet.size < blanks) {
+    blankSet.add(getRandomInt(min, size));
+  }
+  for (let id of blankSet) {
+    gameWords[id] = undefined;
+  }
 
-    console.log(`Returning list of ${gameWords.length}`);
+  console.log(`Returning list of ${gameWords.length}`);
 
-    return gameWords;
+  return gameWords;
 }
 
 var bingoWords = [];
 
 // Read in word list and indicate when we are ready to play
-getWordList('bsbingo.words')
-.then(function(words) {
-    // got word list - store 
+getWordList("bsbingo.words")
+  .then(function(words) {
+    // got word list - store
     bingoWords = words;
-    ipcMain.on('game-request', function(evt, msg) {
-        var game = getGameWords(bingoWords, 15, 7);
-        evt.sender.send('game-data', JSON.stringify(game));
+    ipcMain.on("game-request", function(evt, msg) {
+      var game = getGameWords(bingoWords, 15, 7);
+      evt.sender.send("game-data", JSON.stringify(game));
     });
-})
-.catch(function(err) {
+  })
+  .catch(function(err) {
     console.log(`Error: ${err.message}`);
-    ipcMain.on('game-request', function(evt, msg) {
-        evt.sender.send(JSON.stringify(`Error: ${err.message}`));
+    ipcMain.on("game-request", function(evt, msg) {
+      evt.sender.send(JSON.stringify(`Error: ${err.message}`));
     });
-});
-
+  });
