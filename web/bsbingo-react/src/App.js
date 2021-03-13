e6f0af21c69e1d9d169e673f912ead4d27a67162
Tim Cross <theophilusx@gmail.com>
2021-03-13 18:03:17 +1100

    Update old react dir version

--- web/bsbingo-react/src/App.js
+++ web/bsbingo-react/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import Footer from "./Footer";
+import Game from "./Game";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main>
+        <Dashboard />
+        <div className="game-card" id="game-card">
+          <Game />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
