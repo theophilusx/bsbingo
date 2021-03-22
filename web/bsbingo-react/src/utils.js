bc8420d01ce42845fc194c1c6daedf9e6eb0ed91
Tim Cross <theophilusx@gmail.com>
2021-03-22 19:23:02 +1100

    Add newGame and supporting utility functions

--- web/bsbingo-react/src/utils.js
+++ web/bsbingo-react/src/utils.js
@@ -13,3 +13,33 @@ export function partition(size, data) {
   }
   return result;
 }
+
+export function getRandomInt(min, max) {
+  let minimum = Math.ceil(min);
+  let maximum = Math.floor(max);
+
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+}
+
+export function newGame(wordList, size, blanks) {
+  let wordSet = new Set();
+  let blankIdxSet = new Set();
+
+  while (wordSet.size < size) {
+    wordSet.add(wordList[getRandomInt(0, wordList.length - 1)]);
+  }
+  while (blankIdxSet.size < blanks) {
+    blankIdxSet.add(getRandomInt(0, size - 1));
+  }
+  let words = [...wordSet];
+  for (let i of blankIdxSet) {
+    words[i] = undefined;
+  }
+  return {
+    words,
+    size,
+    blanks,
+    seen: [],
+    complete: false,
+  };
+}
--- web/bsbingo-react/src/utils.test.js
+++ web/bsbingo-react/src/utils.test.js
@@ -1,4 +1,5 @@
 import * as utils from "./utils";
+import { words } from "./words";
 
 it("test partition 1", () => {
   return expect(utils.partition(3, [])).toEqual([]);
@@ -23,4 +24,29 @@ it("Test partition 4", () => {
     [7, 8, 9],
     [10],
   ]);
+});
+
+it("test randomInt", () => {
+  let values = [];
+  let size = 5;
+
+  for (let i = 0; i < size; i++) {
+    values.push(utils.getRandomInt(0, 4));
+    console.log(values);
+  }
+  expect(values.length).toEqual(5);
+  let within = values.filter((v) => v >= 0 && v <= 4);
+  expect(within.length).toEqual(5);
+});
+
+it("test newGame", () => {
+  let game = utils.newGame(words, 20, 5);
+
+  console.dir(game);
+  expect(game.words.length).toEqual(20);
+  expect(game.size).toEqual(20);
+  expect(game.blanks).toEqual(5);
+  expect(game.seen).toEqual([]);
+  let nonBlank = game.words.filter((w) => w !== undefined);
+  return expect(nonBlank.length).toEqual(15);
 });
