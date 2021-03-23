5d3ce6831e3814517fd2af6f4ce8450f4e914074
Tim Cross <theophilusx@gmail.com>
2021-03-24 08:54:05 +1100

    Refactor game card creation

--- web/bsbingo-react/src/Game.js
+++ web/bsbingo-react/src/Game.js
@@ -3,28 +3,16 @@ import GameRow from "./GameRow";
 import { partition } from "./utils";
 
 function Game({ gameWords, seenWordDispatch }) {
-  let [row1, setRow1] = useState([]);
-  let [row2, setRow2] = useState([]);
-  let [row3, setRow3] = useState([]);
-  let [row4, setRow4] = useState([]);
-  let [row5, setRow5] = useState([]);
-
-  useEffect(() => {
-    let groups = partition(5, gameWords);
-    setRow1(groups[0]);
-    setRow2(groups[1]);
-    setRow3(groups[2]);
-    setRow4(groups[3]);
-    setRow5(groups[4]);
-  }, [gameWords]);
-
   return (
     <>
       <div className="card">
-        <GameRow group={row1} seenWordDispatch={seenWordDispatch} />
-        <GameRow group={row2} seenWordDispatch={seenWordDispatch} />
-        <GameRow group={row3} seenWordDispatch={seenWordDispatch} />
-        <GameRow group={row4} seenWordDispatch={seenWordDispatch} />
+        {partition(5, gameWords).map((group, idx) => (
+          <GameRow
+            key={idx}
+            group={group}
+            seenWordDispatch={seenWordDispatch}
+          />
+        ))}
       </div>
     </>
   );
--- web/bsbingo-react/src/GameRow.js
+++ web/bsbingo-react/src/GameRow.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Cell from "./Cell";
+import { strToKey } from "./utils";
 
 function GameRow({ group, seenWordDispatch }) {
   return (
     <div className="row">
       {group.map((word, idx) => (
-        <Cell key={idx} entry={word} seenWordDispatch={seenWordDispatch} />
+        <Cell
+          key={strToKey(word ? word : `blank_${idx}`)}
+          entry={word}
+          seenWordDispatch={seenWordDispatch}
+        />
       ))}
     </div>
   );
