62646660480efe26ce6fc64b59160971d51d08cf
Tim Cross <theophilusx@gmail.com>
2025-09-30 16:24:11 +1000

    Remove package-lock.json

--- web/bsbingo-material/src/Cell.js
+++ web/bsbingo-material/src/Cell.js
@@ -1,27 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { Grid, Paper, Typography, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  makeStyles,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 
 const useStyle = makeStyles((theme) => ({
-  cell: {
-    alignItems: "center",
+  root: {
+    display: "inline-flex",
     textAlign: "center",
-    padding: "1rem 2rrem",
+    justifyContent: "center",
+    alignItems: "center",
     border: "1px solid #859900",
     height: 110,
-    width: 200,
+  },
+  cell: {
+    textAlign: "center",
   },
   blank: {
-    padding: "1rem 2rrem",
-    "test-align": "center",
     "background-image": "linear- gradient(to bottom right, #073642, #002B36)",
   },
   seen: {
-    padding: "1rem 2rrem",
-    "test-align": "center",
-    "background-image": "url(./ check - mark.svg)",
-    "background-position": "right bottom",
-    "background-size": "2rem",
-    "background-repeat": "no-repeat",
+    backgroundImage: "url(./ check - mark.svg)",
+    backgroundPosition: "right bottom",
+    backgroundSize: "2rem",
+    backgroundRepeat: "no-repeat",
   },
 }));
 
@@ -34,25 +39,8 @@ function Cell({ entry, gameDispatch }) {
   }, [entry]);
 
   return (
-    <Grid item>
-      <Paper
-        className={classes.cell}
-        onClick={(e) => {
-          console.log(
-            `Cell clicked: ${entry} State: ${seen ? "seen" : "unseen"}`
-          );
-          e.preventDefault();
-          e.stopPropagation();
-          setSeen(!seen);
-          gameDispatch(
-            seen
-              ? { type: "remove", payload: entry }
-              : { type: "add", payload: entry }
-          );
-        }}
-      >
-        <Typography variant="h6">{entry ? entry : " "}</Typography>
-      </Paper>
+    <Grid item xs={12} sm={6} md={3} className={classes.root}>
+      <Typography variant="h6">{entry ? entry : ""}</Typography>
     </Grid>
   );
 }
--- web/bsbingo-material/src/Game.js
+++ web/bsbingo-material/src/Game.js
@@ -1,14 +1,23 @@
 import React from "react";
-import GameRow from "./GameRow";
+import Cell from "./Cell";
 import { Grid } from "@material-ui/core";
-import { partition } from "./utils";
 
 function Game({ game, gameDispatch }) {
   return (
-    <Grid container item spacing={0} alignItems="stretch">
-      {partition(5, game.words).map((group, idx) => (
-        <GameRow key={idx} group={group} gameDispatch={gameDispatch} />
+    <Grid
+      container
+      item
+      spacing={0}
+      direction="row"
+      alignItems="stretch"
+      justify="center"
+    >
+      {game.words.map((word, idx) => (
+        <Cell key={idx} entry={word} gameDispatch={gameDispatch} />
       ))}
+      {/* {partition(5, game.words).map((group, idx) => ( */}
+      {/*   <GameRow key={idx} group={group} gameDispatch={gameDispatch} /> */}
+      {/* ))} */}
     </Grid>
   );
 }
